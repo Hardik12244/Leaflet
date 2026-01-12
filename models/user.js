68be7da6528244326d7352ab7b67f30ba5c21aed
Hardik12244 <hardik12244@gmail.com>
2026-01-12 23:50:16 +0530

    hashing the password

--- models/user.js
+++ models/user.js
@@ -1,19 +1,53 @@
+const {createHmac, randomBytes} = require("crypto");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const model = mongoose.model;
 
 const userSchema = new Schema({
     fullName:{
         type : String,
-        requried : true,
+        required : true,
     },
     email:{
         type : String,
-        requried: true,
+        required: true,
         unique: true,
+    },
+    salt:{
+        type:String,
+        required:true,
     },
     password:{
         type:String,
         required:true,
-    }
-    
+    },
+    profilePicture:{
+        type:String,
+        default:"/images/profilePic.png"
+    },
+    role:{
+        type:String,
+        enum:["ADMIN","USER"],
+        default:"USER",
+    },
+
 },{timestamps:true})
+
+userSchema.pre("save", function(next){
+    
+    if(!this.user.isModified('password')) return;
+
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac("sha256",salt).update(user.password).digest("hex");
+
+    this.salt = salt;
+    this.password = hashedPassword;
+
+    next();
+})
+
+
+
+const User = model('User',userSchema);
+
+module.exports = User;
