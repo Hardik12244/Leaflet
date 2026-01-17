197aa9abe8fd15cf0d53e7f25c51c0d7235dec8c
Hardik12244 <hardik12244@gmail.com>
2026-01-18 01:10:52 +0530

    date feature added

--- connect.js
+++ connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 
 async function connectMongo(url){
-    return mongoose.connect(url);
+    return  mongoose.connect(url);
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const userRoute = require('./routes/user')
 const blogRoute = require('./routes/blog')
-const {connectMongo} = require('./connect');
+const { connectMongo } = require('./connect');
 const cookieParser = require('cookie-parser');
 const { checkauth } = require('./middlewares/auth');
 const blog = require('./models/blog');
@@ -12,24 +12,27 @@ const app = express();
 const port = 8000;
 app.use(express.static(path.resolve('./public')))
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkauth("token"));
-app.use('/user',userRoute)
-app.use('/blog',blogRoute)
-app.set("view engine","ejs");
-app.set("views",path.resolve("./views"));
+app.use('/user', userRoute)
+app.use('/blog', blogRoute)
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
-app.get('/',async (req,res)=>{
-    const allBlog = await blog.find({});
-    res.render("home",{
-        user:req.user,
-        blogs:allBlog,
+app.get('/', async (req, res) => {
+    const allBlog = await blog.find({}).sort({ createdAt: -1 });;
+    res.render("home", {
+        user: req.user,
+        blogs: allBlog,
     });
 })
+console.log("MONGO =", process.env.MONGO);
 
 connectMongo(process.env.MONGO)
-    .then(()=>console.log("Db connected"))
-    .catch((err)=>console.log("error",err))
+    .then(() => {
+        app.listen(port, () => console.log("Server running on port 8000"));
+        console.log("Db connected")
+    })
+    .catch((err) => console.log("error", err))
 
-app.listen(port,()=> console.log("connected at port 8000"));
